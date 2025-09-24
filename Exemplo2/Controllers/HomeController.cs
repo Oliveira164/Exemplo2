447674721b388ecf54ad335a229d3e2e76e5ceb5
Gustavo <gustavooliveiracosta349@gmail.com>
2025-09-24 16:11:52 -0300

    Criando as homes QuemSomos.cshtml e Contato.cshtml

--- Exemplo2/Controllers/HomeController.cs
+++ Exemplo2/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace Exemplo2.Controllers
             return View();
         }
 
+        public IActionResult QuemSomos()
+        {
+            return View() ;
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
